b175721edb49dce6f12b42b372d1ad1c71739369
Eduardo Dantas <eduardodanttas@icloud.com>
2024-04-06 11:04:08 -0300

    feat: create Loader

---
+++ src/components/loader.tsx
@@ -0,0 +1,7 @@
+import { ActivityIndicator } from "react-native"
+
+export const Loader = () => {
+    return (
+        <ActivityIndicator className="flex-1 bg-green-500 justify-center items-center text-orange-500" />
+    )
+}
